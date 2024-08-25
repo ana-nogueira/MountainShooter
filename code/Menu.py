d933be9d22464498423d235afe92c65e7a52146b
Ana Nogueira <acsnogueira95@gmail.com>
2024-08-25 12:07:32 -0300

    Menu options included

--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH, COLOR_ORANGE
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
@@ -20,6 +20,10 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50,'Mountain',(COLOR_ORANGE),((WIN_WIDTH / 2), 70))
             self.menu_text(50, 'Shooter', (COLOR_ORANGE), ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20,MENU_OPTION[i], COLOR_WHITE,((WIN_WIDTH / 2), 200 + 25 * i))
+
             pygame.display.flip()
 
             # Check for all events
